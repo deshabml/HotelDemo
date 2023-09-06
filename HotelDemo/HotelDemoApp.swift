17d400946b67345abb77b0166abe11dc23228425
deshabml <deshab@yandex.ru>
2023-09-06 09:21:46 +0300

    Initial Commit

---
+++ HotelDemo/HotelDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelDemoApp.swift
+//  HotelDemo
+//
+//  Created by Лаборатория on 06.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
