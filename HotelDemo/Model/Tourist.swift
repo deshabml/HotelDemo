b6bedddae4e019f66638ba7d0e1b114eab0bbbac
deshabml <deshab@yandex.ru>
2023-09-09 08:55:03 +0300

    Added Tourist model

---
+++ HotelDemo/Model/Tourist.swift
@@ -0,0 +1,20 @@
+//
+//  Tourist.swift
+//  HotelDemo
+//
+//  Created by Лаборатория on 09.09.2023.
+//
+
+import Foundation
+
+struct Tourist: Identifiable, Hashable {
+
+    let id = UUID().uuidString
+    var name: String
+    var surname: String
+    var dateOfBirth: String
+    var citizenship: String
+    var passportNumber: String
+    var validityPeriodOfPassport: String
+    
+}
